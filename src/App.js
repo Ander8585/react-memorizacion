2ef58cf84566aac2785cb0cb90ded3fbf23a6240
Ander Torres <ander8585@gmail.com>
2021-10-28 19:24:48 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Contador from "./components/Contador";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Memorizacion en React</h1>
+			<hr />
+			<h2>Teoria</h2>
+			<h3>
+				<a
+					href="https://es.reactjs.org/docs/react-api.html#reactmemo"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Memo
+				</a>
+			</h3>
+			<ul>
+				<li>Se encarga de memorizar un componente</li>
+				<li>
+					Lo vuelve a memorizar al momento de que sus <b>props</b> cambian
+				</li>
+				<li>Evita re-renderizados</li>
+				<li>
+					Hay que evitarlo en la medida de lo posible, pues podria ser mas
+					costosa la tarea de memorizacion que el re-renderizado del componente.
+				</li>
+				<li>
+					Usalo cuando:
+					<ul>
+						<li>Tenemos muchos elementos renderizados en una lista</li>
+						<li>Llamamos datos de APIs</li>
+						<li>Un componente se vuelve muy pesado.</li>
+						<li>Salen alertas de rendimiento en la consola</li>
+					</ul>
+				</li>
+			</ul>
+			<h3>
+				<a
+					href="https://es.reactjs.org/docs/hooks-reference.html#usecallback"
+					target="_blank"
+					rel="noreferrer"
+				>
+					useCallback
+				</a>
+			</h3>
+			<ul>
+				<li>Memoriza una funcion para no volverla a definir en cada render.</li>
+				<li>
+					Usalo siempre que se pase una funcion como <b>prop</b> a un componente
+					memorizado.
+				</li>
+				<li>
+					Usalo siempre qeu se pase una funcion como parametro de un efecto.
+				</li>
+			</ul>
+			<h3>
+				<a
+					href="https://es.reactjs.org/docs/hooks-reference.html#usememo"
+					target="_blank"
+					rel="noreferrer"
+				>
+					useMemo
+				</a>
+			</h3>
+			<ul>
+				<li>
+					Memoriza un valor calculo, es decir, el resultado de una funcion.
+				</li>
+				<li>Genera propiedades computadas.</li>
+				<li>Usalo en procesos pesados.</li>
+			</ul>
+			<hr />
+			<Contador />
+		</div>
+	);
 }
 
 export default App;
